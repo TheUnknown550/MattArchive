9ac817594f1a9c5bc7767dc9bcbe5a5b1e30ec9c
TheUnknown550 <85489536+TheUnknown550@users.noreply.github.com>
2021-10-27 22:57:40 +0700

    adw

---
+++ TextToSpeach.py
@@ -0,0 +1,15 @@
+import pyaudio
+import speech_recognition as sr
+mic = sr.Microphone(1)
+mic
+recog = sr.Recognizer()
+recog
+with mic as source:
+ while True:
+  print("listening")
+  audio = recog.listen(source)
+  print("Calibrating")
+  try:
+   print("you said: ",recog.recognize_google(audio,language='th'))   
+  except:
+   continue
